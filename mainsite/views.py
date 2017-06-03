0be26636eba3c698fde5f957b5459332095f9bd0
Tangcheng <helloworld.tang@qq.com>
2017-06-04 00:08:04 +0800

    url- Jumps changes to url

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import print_function
+from __future__ import print_function
+from __future__ import print_function
 from __future__ import unicode_literals
 
+import traceback
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template.loader import get_template
 
@@ -17,3 +22,20 @@ def homepage(request):
     now = datetime.now()
     html = template.render(locals())
     return HttpResponse(html)
+
+
+def showpost(request, slug):
+    template = get_template("post.html")
+    try:
+        post = Post.objects.get(slug=slug)
+        if post is not None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except  Exception as e:
+        print('str(Exception):\t', str(Exception))
+        print('str(e):\t\t', str(e))
+        print('repr(e):\t', repr(e))
+        print('e.message:\t', e.message)
+        print('traceback.print_exc():', traceback.print_exc())
+        print('traceback.format_exc():\n%s' % traceback.format_exc())
+        return redirect("/")
